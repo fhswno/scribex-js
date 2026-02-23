5e75f0ddedb9db133851eb884bdcf44dfaef251e
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-23 03:56:16 +0100

    clean

--- packages/scribex/src/commands.ts
+++ packages/scribex/src/commands.ts
@@ -2,3 +2,4 @@
 import { createCommand } from 'lexical';
 
 export const OPEN_SLASH_MENU_COMMAND = createCommand<void>('OPEN_SLASH_MENU_COMMAND');
+export const INSERT_IMAGE_COMMAND = createCommand<File>('INSERT_IMAGE_COMMAND');
