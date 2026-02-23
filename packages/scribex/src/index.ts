66b05f6f0996e79ae6e27a268cb0c4d3cdba315c
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-23 02:50:43 +0100

    index pacakge

---
+++ packages/scribex/src/index.ts
@@ -0,0 +1,8 @@
+export { EditorRoot } from './components/EditorRoot';
+export { FloatingToolbar } from './components/FloatingToolbar';
+export { SlashMenu } from './components/SlashMenu';
+export { InputRulePlugin } from './plugins/InputRulePlugin';
+export { useEditorState } from './hooks/useEditorState';
+export { OPEN_SLASH_MENU_COMMAND } from './commands';
+export type { UploadHandler, MentionProvider, MentionItem, InputRule } from './types';
+export type { SlashMenuItem } from './components/SlashMenu';
