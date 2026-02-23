59915638036f83243d42279944a097031be5030f
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-23 03:22:33 +0100

    export OverlayPortal from components in index.ts

--- packages/scribex/src/index.ts
+++ packages/scribex/src/index.ts
@@ -1,6 +1,7 @@
 export { EditorRoot } from './components/EditorRoot';
 export { FloatingToolbar } from './components/FloatingToolbar';
 export { SlashMenu } from './components/SlashMenu';
+export { OverlayPortal } from './components/OverlayPortal';
 export { InputRulePlugin } from './plugins/InputRulePlugin';
 export { useEditorState } from './hooks/useEditorState';
 export { OPEN_SLASH_MENU_COMMAND } from './commands';
