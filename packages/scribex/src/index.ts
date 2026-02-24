bf5e02d43ae4e693adf68db49d55dc7067e54714
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-24 13:28:24 +0100

    add MentionNode and MentionPayload exports to index

--- packages/scribex/src/index.ts
+++ packages/scribex/src/index.ts
@@ -5,11 +5,13 @@ export { OverlayPortal } from './components/OverlayPortal';
 export { InputRulePlugin } from './plugins/InputRulePlugin';
 export { ImagePlugin } from './plugins/ImagePlugin';
 export { AIPlugin } from './plugins/AIPlugin';
+export { MentionPlugin } from './plugins/MentionPlugin';
 export { useEditorState } from './hooks/useEditorState';
 export { OPEN_SLASH_MENU_COMMAND, INSERT_IMAGE_COMMAND, OPEN_AI_PROMPT_COMMAND, INSERT_AI_PREVIEW_COMMAND } from './commands';
 export { ImageNode, $createImageNode, $isImageNode } from './nodes/ImageNode';
 export { LoadingImageNode, $createLoadingImageNode, $isLoadingImageNode } from './nodes/LoadingImageNode';
 export { AIPreviewNode, $createAIPreviewNode, $isAIPreviewNode } from './nodes/AIPreviewNode';
+export { MentionNode, $createMentionNode, $isMentionNode } from './nodes/MentionNode';
 export { serializeNodesToMarkdown, $parseMarkdownToLexicalNodes } from './utils/markdown';
 export type {
   UploadHandler,
@@ -27,3 +29,4 @@ export type {
 export type { SlashMenuItem } from './components/SlashMenu';
 export type { ImagePayload } from './nodes/ImageNode';
 export type { AIPreviewPayload } from './nodes/AIPreviewNode';
+export type { MentionPayload } from './nodes/MentionNode';
