60b95ae8cca851d98fbc9bc1eaac2e436966fadb
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-25 00:59:00 +0100

    feat: add TableNode, TableRowNode, and TableCellNode to ALL_NODES export

--- packages/scribex/src/nodes/index.ts
+++ packages/scribex/src/nodes/index.ts
@@ -2,6 +2,7 @@ import type { Klass, LexicalNode } from 'lexical';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 import { ListNode, ListItemNode } from '@lexical/list';
 import { LinkNode, AutoLinkNode } from '@lexical/link';
+import { TableNode, TableRowNode, TableCellNode } from '@lexical/table';
 import { ImageNode } from './ImageNode';
 import { LoadingImageNode } from './LoadingImageNode';
 import { AIPreviewNode } from './AIPreviewNode';
@@ -15,6 +16,9 @@ export const ALL_NODES: Array<Klass<LexicalNode>> = [
   ListItemNode,
   LinkNode,
   AutoLinkNode,
+  TableNode,
+  TableRowNode,
+  TableCellNode,
   ImageNode,
   LoadingImageNode,
   AIPreviewNode,
