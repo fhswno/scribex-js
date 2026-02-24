481ae2847f928ab01c8179e3e4f305444972258a
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-24 13:18:59 +0100

    update index.ts to export AI-related commands and types

--- packages/scribex/src/index.ts
+++ packages/scribex/src/index.ts
@@ -4,10 +4,26 @@ export { SlashMenu } from './components/SlashMenu';
 export { OverlayPortal } from './components/OverlayPortal';
 export { InputRulePlugin } from './plugins/InputRulePlugin';
 export { ImagePlugin } from './plugins/ImagePlugin';
+export { AIPlugin } from './plugins/AIPlugin';
 export { useEditorState } from './hooks/useEditorState';
-export { OPEN_SLASH_MENU_COMMAND, INSERT_IMAGE_COMMAND } from './commands';
+export { OPEN_SLASH_MENU_COMMAND, INSERT_IMAGE_COMMAND, OPEN_AI_PROMPT_COMMAND, INSERT_AI_PREVIEW_COMMAND } from './commands';
 export { ImageNode, $createImageNode, $isImageNode } from './nodes/ImageNode';
 export { LoadingImageNode, $createLoadingImageNode, $isLoadingImageNode } from './nodes/LoadingImageNode';
-export type { UploadHandler, MentionProvider, MentionItem, InputRule } from './types';
+export { AIPreviewNode, $createAIPreviewNode, $isAIPreviewNode } from './nodes/AIPreviewNode';
+export { serializeNodesToMarkdown, $parseMarkdownToLexicalNodes } from './utils/markdown';
+export type {
+  UploadHandler,
+  MentionProvider,
+  MentionItem,
+  InputRule,
+  AIProvider,
+  AIGenerateConfig,
+  AIGenerateParams,
+  AIPluginConfig,
+  AIPreviewLabels,
+  AIRetryConfig,
+  AIPromptInputRenderProps,
+} from './types';
 export type { SlashMenuItem } from './components/SlashMenu';
 export type { ImagePayload } from './nodes/ImageNode';
+export type { AIPreviewPayload } from './nodes/AIPreviewNode';
