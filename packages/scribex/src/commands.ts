fc730091d5d85afc6e74c5cc66a49ecdc881038f
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-25 00:12:27 +0100

    emoji picker

--- packages/scribex/src/commands.ts
+++ packages/scribex/src/commands.ts
@@ -5,3 +5,5 @@ export const OPEN_SLASH_MENU_COMMAND = createCommand<void>('OPEN_SLASH_MENU_COMM
 export const INSERT_IMAGE_COMMAND = createCommand<File>('INSERT_IMAGE_COMMAND');
 export const OPEN_AI_PROMPT_COMMAND = createCommand<void>('OPEN_AI_PROMPT_COMMAND');
 export const INSERT_AI_PREVIEW_COMMAND = createCommand<{ prompt: string }>('INSERT_AI_PREVIEW_COMMAND');
+export const OPEN_EMOJI_PICKER_COMMAND = createCommand<void>('OPEN_EMOJI_PICKER_COMMAND');
+export const OPEN_LINK_INPUT_COMMAND = createCommand<void>('OPEN_LINK_INPUT_COMMAND');
