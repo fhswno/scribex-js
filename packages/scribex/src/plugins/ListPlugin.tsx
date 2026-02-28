9e7619f43d2783dbb2d949369137f549c1ab8b27
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-28 16:05:20 +0100

    feat: add ListPlugin to handle list behavior in the editor

---
+++ packages/scribex/src/plugins/ListPlugin.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { registerList } from "@lexical/list";
+
+/**
+ * Registers @lexical/list behaviour: Enter on an empty list item exits the list,
+ * Tab/Shift-Tab for indent/outdent, and other list integrity transforms.
+ */
+export function ListPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    return registerList(editor);
+  }, [editor]);
+
+  return null;
+}
