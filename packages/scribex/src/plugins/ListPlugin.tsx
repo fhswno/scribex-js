40200238eafc287847f7338782ffb79c6ef55854
Dave Ohayon <davidohn.uk@hotmail.com>
2026-02-28 19:14:36 +0100

    list plugin

--- packages/scribex/src/plugins/ListPlugin.tsx
+++ packages/scribex/src/plugins/ListPlugin.tsx
@@ -5,17 +5,23 @@ import { useEffect } from "react";
 
 // LEXICAL
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { registerList } from "@lexical/list";
+import { registerList, registerCheckList } from "@lexical/list";
 
 /**
  * Registers @lexical/list behaviour: Enter on an empty list item exits the list,
  * Tab/Shift-Tab for indent/outdent, and other list integrity transforms.
+ * Also registers check list behaviour: click/spacebar to toggle checkbox state.
  */
 export function ListPlugin() {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
-    return registerList(editor);
+    const unregisterList = registerList(editor);
+    const unregisterCheckList = registerCheckList(editor);
+    return () => {
+      unregisterList();
+      unregisterCheckList();
+    };
   }, [editor]);
 
   return null;
