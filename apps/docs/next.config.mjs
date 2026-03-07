3c141788a13ee9732a4e54fb0fdf8d0f3098e96d
Dave Ohayon <davidohn.uk@hotmail.com>
2026-03-07 22:58:16 +0100

    next config

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,9 @@
+// FUMADOCS
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {};
+
+export default withMDX(config);
